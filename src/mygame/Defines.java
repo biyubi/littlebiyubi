9b14dc6815f062b2b9c152cc8cdbb9b1a3ad6c46
edba <edba.dev@gmail.com>
2012-04-26 20:48:11 -0700

    turn off cheats.

--- src/mygame/Defines.java
+++ src/mygame/Defines.java
@@ -17,7 +17,7 @@ public class Defines {
     public static final int TOTAL_LEVELS            = 1;
     
     //Cheats
-    public static final boolean CHEAT_ALL_ABILITIES = true;
+    public static final boolean CHEAT_ALL_ABILITIES = false;
     
     //Debug
     public static final boolean DEBUG_COLLISIONS    = false;
